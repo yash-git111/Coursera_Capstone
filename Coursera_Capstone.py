7aab26e6c810c075e40a272392e7ec7078327bae
yash-git111 <70102531+yash-git111@users.noreply.github.com>
2020-09-24 13:01:22 +0530

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook is used mainly for capstone project.
+
+# In[ ]:
+
+
+Import pandas as pd
+
+
+# In[ ]:
+
+
+Import numpy as np
+
+
+# In[1]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
